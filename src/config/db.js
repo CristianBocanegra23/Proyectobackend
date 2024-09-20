7534dc8ac45270347d614d65aadc387a9272ecb9
Cristian Bocanegra <Bocanegrac8@gmail.com>
2024-09-20 06:14:25 -0500

    Actualizacion de Pool con pg

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
+const { Pool } = require("pg");
+let pool = Pool;
 
-const { Pool } = require('pg');
+if (process.env.PRODUCTION == "true") {
+  pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 5432,
+  });
+}
+module.exports = pool;
+
+/*const { Pool } = require('pg');
 const pool = new Pool({
 host: process.env.DB_HOST,
 user: process.env.DB_USER,
 password: process.env.DB_PASSWORD,
 database: process.env.DB_NAME,
-port:5432
-});
-module.exports=pool;
+port:5432})*/
